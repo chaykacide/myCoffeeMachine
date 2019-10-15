e0fe88e8bb2d36e8f5caa7048aa62e244fdb20c2
chaykacide <55700455+chaykacide@users.noreply.github.com>
2019-10-15 16:03:06 +0300

    Add files via upload

--- G74L4/CoffeeMachine.swift
+++ G74L4/CoffeeMachine.swift
@@ -5,161 +5,161 @@ class CoffeeMachine: NSObject {
         self.name = name
     }
     
-    struct MyVariables {
+    struct CoffeeMachineTanks {
         static var waterTank = 500
         static var milkTank = 300
         static var beansTank = 50
     }
     
-    let waterForCapuchino = 200
-    let milkForCapuchino = 125
-    let beansForCapuchino = 15
+    private let waterForCapuchino = 200
+    private let milkForCapuchino = 125
+    private let beansForCapuchino = 15
     
-    let waterForAmericano = 125
-    let milkForAmericano = 0
-    let beansForAmericano = 12
+    private let waterForAmericano = 125
+    private let milkForAmericano = 0
+    private let beansForAmericano = 12
     
-    let waterForLatte = 250
-    let milkForLatte = 150
-    let beansForLatte = 17
+    private let waterForLatte = 250
+    private let milkForLatte = 150
+    private let beansForLatte = 17
     
-    let waterForMokko = 150
-    let milkForMokko = 225
-    let beansForMokko = 11
+    private let waterForMokko = 150
+    private let milkForMokko = 225
+    private let beansForMokko = 11
     
     func makeCapuchino() -> String {
         var done = ""
-        if MyVariables.waterTank >= waterForCapuchino &&
-            MyVariables.milkTank >= milkForCapuchino &&
-            MyVariables.beansTank >= beansForCapuchino {
+        if CoffeeMachineTanks.waterTank >= waterForCapuchino &&
+            CoffeeMachineTanks.milkTank >= milkForCapuchino &&
+            CoffeeMachineTanks.beansTank >= beansForCapuchino {
             done = cupOfCapuchino()
         }
-        else if MyVariables.waterTank < waterForCapuchino {
+        else if CoffeeMachineTanks.waterTank < waterForCapuchino {
             done = "Не хватает воды."
         }
-        else if MyVariables.milkTank < milkForCapuchino {
+        else if CoffeeMachineTanks.milkTank < milkForCapuchino {
             done = "Не хватает молока."
         }
-        else if MyVariables.beansTank < beansForCapuchino {
+        else if CoffeeMachineTanks.beansTank < beansForCapuchino {
             done = "Не хватает зёрен."
         }
         return done
     }
     
     func cupOfCapuchino() -> String {
-        MyVariables.waterTank -= waterForCapuchino
-        MyVariables.milkTank -= milkForCapuchino
-        MyVariables.beansTank -= beansForCapuchino
+        CoffeeMachineTanks.waterTank -= waterForCapuchino
+        CoffeeMachineTanks.milkTank -= milkForCapuchino
+        CoffeeMachineTanks.beansTank -= beansForCapuchino
         return "Ваше Капучино, сэр!"
     }
     
     func makeAmericano() -> String {
         var done = ""
-        if MyVariables.waterTank >= waterForAmericano &&
-            MyVariables.milkTank >= milkForAmericano &&
-            MyVariables.beansTank >= beansForAmericano {
+        if CoffeeMachineTanks.waterTank >= waterForAmericano &&
+            CoffeeMachineTanks.milkTank >= milkForAmericano &&
+            CoffeeMachineTanks.beansTank >= beansForAmericano {
             done = cupOfAmericano()
-        } else if MyVariables.waterTank < waterForAmericano {
+        } else if CoffeeMachineTanks.waterTank < waterForAmericano {
             done = "Не хватает воды."
-        } else if MyVariables.milkTank < milkForAmericano {
+        } else if CoffeeMachineTanks.milkTank < milkForAmericano {
             done = "Не хватает молока."
-        } else if MyVariables.beansTank < beansForAmericano {
+        } else if CoffeeMachineTanks.beansTank < beansForAmericano {
             done = "Не хватает зёрен."
         }
         return done
     }
     
     func cupOfAmericano() -> String {
-        MyVariables.waterTank -= waterForAmericano
-        MyVariables.milkTank -= milkForAmericano
-        MyVariables.beansTank -= beansForAmericano
+        CoffeeMachineTanks.waterTank -= waterForAmericano
+        CoffeeMachineTanks.milkTank -= milkForAmericano
+        CoffeeMachineTanks.beansTank -= beansForAmericano
         return "Ваше американо, сэр!"
     }
     
     func makeLatte() -> String {
         var done = ""
-        if MyVariables.waterTank >= waterForLatte &&
-            MyVariables.milkTank >= milkForLatte &&
-            MyVariables.beansTank >= beansForLatte {
+        if CoffeeMachineTanks.waterTank >= waterForLatte &&
+            CoffeeMachineTanks.milkTank >= milkForLatte &&
+            CoffeeMachineTanks.beansTank >= beansForLatte {
             done = cupOfLatte()
-        } else if MyVariables.waterTank < waterForLatte {
+        } else if CoffeeMachineTanks.waterTank < waterForLatte {
             done = "Не хватает воды."
-        } else if MyVariables.milkTank < milkForLatte {
+        } else if CoffeeMachineTanks.milkTank < milkForLatte {
             done = "Не хватает молока."
-        } else if MyVariables.beansTank < beansForLatte {
+        } else if CoffeeMachineTanks.beansTank < beansForLatte {
             done = "Не хватает зёрен."
         }
         return done
     }
     
     func cupOfLatte() -> String {
-        MyVariables.waterTank -= waterForLatte
-        MyVariables.milkTank -= milkForLatte
-        MyVariables.beansTank -= beansForLatte
+        CoffeeMachineTanks.waterTank -= waterForLatte
+        CoffeeMachineTanks.milkTank -= milkForLatte
+        CoffeeMachineTanks.beansTank -= beansForLatte
         return "Ваше латтэ, сэр!"
     }
     
     func makeMokko() -> String {
         var done = ""
-        if MyVariables.waterTank >= waterForMokko &&
-            MyVariables.milkTank >= milkForMokko &&
-            MyVariables.beansTank >= beansForMokko {
+        if CoffeeMachineTanks.waterTank >= waterForMokko &&
+            CoffeeMachineTanks.milkTank >= milkForMokko &&
+            CoffeeMachineTanks.beansTank >= beansForMokko {
             done = cupOfMokko()
-        } else if MyVariables.waterTank < waterForMokko {
+        } else if CoffeeMachineTanks.waterTank < waterForMokko {
             done = "Не хватает воды."
-        } else if MyVariables.milkTank < milkForMokko {
+        } else if CoffeeMachineTanks.milkTank < milkForMokko {
             done = "Не хватает молока."
-        } else if MyVariables.beansTank < beansForMokko {
+        } else if CoffeeMachineTanks.beansTank < beansForMokko {
             done = "Не хватает зёрен."
         }
         return done
     }
     
     func cupOfMokko() -> String {
-        MyVariables.waterTank -= waterForMokko
-        MyVariables.milkTank -= milkForMokko
-        MyVariables.beansTank -= beansForMokko
+        CoffeeMachineTanks.waterTank -= waterForMokko
+        CoffeeMachineTanks.milkTank -= milkForMokko
+        CoffeeMachineTanks.beansTank -= beansForMokko
         return "Ваше мокко, сэр!"
     }
     
     func addWater() -> String {
         var done = ""
-        if MyVariables.waterTank >= 500 {
+        if CoffeeMachineTanks.waterTank >= 500 {
             done = "Некуда лить, бак полон."
         }
-        while MyVariables.waterTank < 500 {
-            MyVariables.waterTank += 1
-            done = "Вы долили воды, теперь в баке \(MyVariables.waterTank) мл."
+        while CoffeeMachineTanks.waterTank < 500 {
+            CoffeeMachineTanks.waterTank += 1
+            done = "Вы долили воды, теперь в баке \(CoffeeMachineTanks.waterTank) мл."
         }
         return done
     }
     
     func addMilk() -> String {
         var done = ""
-        if MyVariables.milkTank >= 300 {
+        if CoffeeMachineTanks.milkTank >= 300 {
             done = "Некуда лить, бак полон."
         }
-        while MyVariables.milkTank < 300 {
-            MyVariables.milkTank += 1
-            done = "Вы долили молока, теперь в баке \(MyVariables.milkTank) мл."
+        while CoffeeMachineTanks.milkTank < 300 {
+            CoffeeMachineTanks.milkTank += 1
+            done = "Вы долили молока, теперь в баке \(CoffeeMachineTanks.milkTank) мл."
         }
         return done
     }
     
     func addBeans() -> String {
         var done = ""
-        if MyVariables.beansTank >= 50 {
+        if CoffeeMachineTanks.beansTank >= 50 {
             done = "Некуда сыпать, лоток полон."
         }
-        while MyVariables.beansTank < 50 {
-            MyVariables.beansTank += 1
-            done = "Вы засыпали зёрен, теперь в лотке \(MyVariables.beansTank) шт."
+        while CoffeeMachineTanks.beansTank < 50 {
+            CoffeeMachineTanks.beansTank += 1
+            done = "Вы засыпали зёрен, теперь в лотке \(CoffeeMachineTanks.beansTank) шт."
         }
         return done
     }
 
     func ingrStatusChecker() -> String {
-        let sendIngrStatus = "Осталось: \(MyVariables.waterTank) мл воды, \(MyVariables.milkTank) мл молока и \(MyVariables.beansTank) зёрен."
+        let sendIngrStatus = "Осталось: \(CoffeeMachineTanks.waterTank) мл воды, \(CoffeeMachineTanks.milkTank) мл молока и \(CoffeeMachineTanks.beansTank) зёрен."
         return sendIngrStatus
     }
     
